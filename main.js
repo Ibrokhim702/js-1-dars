bccc30063d1e794dc5fb9adc668236d311adcb0a
Ibrokhim702 <rakhimovaziz077@gmail.com>
2024-12-04 20:57:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// console.log(11,16);
+// console.log(90);
+// console.log("tom");
+
+// console.log("murod"+"qudrat");
+// console.log("5"+"5");
+// console.log(5+5+"5");
+// var ism ="tom"
+// var yosh = "15";
+// console.log(ism,yosh);
+// var username = prompt("ismingizni yozing,")
+// var usersurname = +prompt("yosh")
+// var familiya = +prompt("familiya")
+// console.log(username,usersurname,familiya);
+// var num = prompt ("ikkinchi")
+
+var ism = prompt("Ismingizni kiriting")
+var surname = prompt("Familiyangiz kiriting")
+var age = prompt("Yoshingizni kiriting")
+var subject = prompt("Yoqtirgan faningizni kiriting")
+var home = prompt("Yashaydigan tumaningizni kiriting")
+
+console.log(`ismingiz:${ism}`);
+console.log(`Familiyangiz:${surname}`);
+console.log(`Yoshiz:${age}`);
+console.log(`Yoqtirgan faninigiz:${subject}`);
+console.log(`Yashaydigan tumaningiz:${home}`);
+
+
+
+
